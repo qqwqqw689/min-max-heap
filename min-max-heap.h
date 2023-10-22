c62fb25cbafd060b6a36883ec7f1252771f10372
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-22 15:52:00 +0800

    updates

--- min-max-heap.h
+++ min-max-heap.h
@@ -1,5 +1,6 @@
 // http://akira.ruc.dk/~keld/teaching/algoritmedesign_f03/Artikler/02/Atkinson86.pdf
 #include <cmath>
+#include <vector>
 
 #define LeftChild(i) i<<1
 #define RightChild(i) i<<1 + 1
@@ -7,3 +8,33 @@
 #define GrandParent(i) i>>2
 #define Level(i) int(std::log2(i))
 
+// values stored at nodes on even levels are smaller than or equal to the values
+// stored at their descendants
+
+template<typename T>
+class MinMaxHeap
+{
+private:
+    std::vector<T> allocator;
+    int _HeapSize;
+
+    void TrickleDownMin(int i);
+    void TrickleDownMax(int i);
+public:
+    void TrickleDown(int i);
+};
+
+template<typename T>
+void MinMaxHeap<T>::TrickleDown(int i)
+{
+    if(Level(i)&1)
+        TrickleDownMax(i);
+    else
+        TrickleDownMin(i);
+}
+
+template<typename T>
+void MinMaxHeap<T>::TrickleDownMin(int i)
+{
+
+}
